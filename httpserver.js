292f5d9c562e88ec5fc4d06149608539da822818
Dave Eddy <dave@daveeddy.com>
2013-04-29 15:52:25 -0700

    rely on static-route

--- httpserver.js
+++ httpserver.js
@@ -7,15 +7,12 @@
  * License: MIT
  */
 
-var fs = require('fs');
 var http = require('http');
 var os = require('os');
-var path = require('path');
-var url = require('url');
 
 var accesslog = require('access-log');
 var easyreq = require('easyreq');
-var mime = require('mime');
+var staticroute = require('static-route')({autoindex: true});
 
 var host = process.argv[3] || process.env.NODE_HOST || '0.0.0.0';
 var port = +process.argv[2] || +process.env.NODE_PORT || 8080;
@@ -34,63 +31,7 @@ function listening() {
 function onrequest(req, res) {
   easyreq(req, res);
   accesslog(req, res);
-
-  var file = path.join(process.cwd(), decodeURI(req.urlparsed.pathname));
-
-  // the user wants some actual data
-  fs.stat(file, function(err, stats) {
-    if (err) {
-      console.error(err.message);
-      res.notfound();
-      return;
-    }
-
-    if (stats.isDirectory()) {
-      // dir
-      fs.readdir(file, function(e, ret) {
-        if (e) {
-          console.error(e.message);
-          res.error();
-          return;
-        }
-        ret.sort();
-        ret = ['.', '..'].concat(ret);
-        if (req.urlparsed.query.hasOwnProperty('json')) {
-          res.setHeader('Content-Type', 'application/json; charset=utf-8');
-          res.end(JSON.stringify(ret));
-        } else {
-          res.setHeader('Content-Type', 'text/html; charset=utf-8');
-          res.write('<ul>\n');
-          ret.forEach(function(name) {
-            res.write('<li>' + name.link(path.join(req.urlparsed.pathname, name)) + '</li>\n');
-          });
-          res.write('</ul>');
-          res.end();
-        }
-      });
-    } else {
-      // file
-      var etag = '"' + stats.size + '-' + Date.parse(stats.mtime) + '"';
-      res.setHeader('Last-Modified', stats.mtime);
-
-      // check cache
-      if (req.headers['if-none-match'] === etag) {
-        res.statusCode = 304;
-        res.end();
-      } else {
-        res.setHeader('Content-Length', stats.size);
-        res.setHeader('Content-Type', mime.lookup(file));
-        res.setHeader('ETag', etag);
-        if (req.method === 'HEAD') {
-          res.end();
-        } else {
-          var rs = fs.createReadStream(file);
-          rs.pipe(res);
-          res.on('close', rs.destroy.bind(rs));
-        }
-      }
-    }
-  });
+  staticroute(req, res);
 }
 
 // get all ipv4 addresses
